c6ad9d90114ffff1d7f748bc7455078bf5a5ad29
AshangiCode <163718692+AshangiCode@users.noreply.github.com>
2025-03-07 14:07:45 +0700

    Story-Guide in Menu

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -1,7 +1,33 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainmenu;
+    public GameObject guide;
+    public GameObject story;
+
+    public Button nextGuideButton;
+    public Button nextStoryButton;
+
+    void Start()
+    {
+        nextGuideButton.onClick.AddListener(StartGuide);
+        nextStoryButton.onClick.AddListener(StartStory);
+    }
+    public void StartGuide()
+    {        
+        guide.SetActive(true);
+        mainmenu.SetActive(false);
+        story.SetActive(false);
+    }
+
+    public void StartStory()
+    {
+        mainmenu.SetActive(false);
+        guide.SetActive(false);
+        story.SetActive(true);
+    }
 
     public void PlayGame()
     {
